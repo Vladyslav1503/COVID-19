9e6d940468fd6b74485a2efcc49eda810dd722d7
MoroMannKristian <kristin.isaksen03@gmail.com>
2021-02-16 11:26:00 +0100

    Update modell.py

--- project/modell.py
+++ project/modell.py
@@ -3,3 +3,16 @@ import numpy as np
 import pygame
 import cv2
 
+x = 1
+y = 1
+All_infected = True
+
+while x < 1000:
+    image = cv2.imread("./VerdenPopMindre.png")
+    p = image[x,y]
+    white = np.array([255, 255, 255])
+    comparioson_white = p == white
+    if not comparioson_white.all():
+        print(p)
+    x += 1
+    y += 1
