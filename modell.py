178363290ccba5ac548d9e8a966e0fa15091bfaa
MoroMannKristian <kristin.isaksen03@gmail.com>
2021-02-19 10:19:53 +0100

    Create modell.py

---
+++ modell.py
@@ -0,0 +1,31 @@
+import math
+import numpy as np
+import cv2
+
+class coronaSIR:
+    pop = 1 #Verdens populasjon
+    Y = 0.1 #Hvor lang tid det tar før en passient bblir frisk
+    deltaT = 1
+    
+    def __init__(self, sted, sted_populasjon, beta):
+        self.name = sted #Område navn. 
+        self.pop = sted_populasjon #Populasjonen til det område
+        self.beta = beta #Beta verdien er hvor ofte folk omgås i det område
+        self.suseptible = [sted_populasjon] #Hvor mange som er "Suseptible" i område
+        self.infected = [] #Hvor mange som er "infected" i område
+        self.recoverd = [] #Hvor mange som har "recoverd" i område
+        
+    def ny_S(self): #Sette ny verdi til "suseptible"
+        self.suseptible.append(self.suseptible[-1] + 
+            ((-self.beta*self.suseptible[-1]*self.infected[-1])/self.pop))
+            #Funksjonen for å finne ut ny S: S - (B * S * I)/Pop
+        
+    def ny_I(self): #Sette ny verdie til "infected"
+        self.infected.append(self.infected[-1] + 
+            (((self.beta*self.suseptible[-2]*self.infected[-1])/self.pop)
+            - Y*self.infected[-1]))
+            #Funksjon for å finne ut ny I: I + (B * S * I)/Pop - (Y * I)
+    
+    def ny_R(self): #Sette ny verdi til "recoverd"
+        self.recoverd.append(self.recoverd + (Y*self.infected[-2]))
+            #Funksjon for å finne ut ny R: R + (Y * I)
