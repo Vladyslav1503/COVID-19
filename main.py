ddb792890b1b5ed68d378512682df79d74e84bda
Vladyslav1503 <49617487+Vladyslav1503@users.noreply.github.com>
2021-03-03 12:28:27 +0100

    update

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.preprocessing import image_preprocessing
 
 IMAGE_SRC = "./src/images/VerdenPopMindre.png"
 
-population_points = image_preprocessing(IMAGE_SRC)
+population_points, borders = image_preprocessing(IMAGE_SRC)
--- src/modell.py
+++ src/modell.py
@@ -27,29 +27,28 @@ class coronaSIR:
         # - Y*self.infected[-1]))
         # Funksjon for å finne ut ny I: I + (B * S * I)/Pop - (Y * I)
 
+        # def ny_R(self): #Sette ny verdi til "recoverd"
+        # self.recoverd.append(self.recoverd[-1] + (Y*self.infected[-2]))
+        # Funksjon for å finne ut ny R: R + (Y * I)
+
+        self.posisjon = posisjon  # Område navn.
+        self.pop = sted_populasjon  # Populasjonen til det område
+        self.beta = beta  # Beta verdien er hvor ofte folk omgås i det område
+        self.suseptible = [sted_populasjon]  # Hvor mange som er "Suseptible" i område
+        self.infected = [0]  # Hvor mange som er "infected" i område
+        # self.recoverd = [0] #Hvor mange som har "recoverd" i område
+
+    def ny_S(self):  # Sette ny verdi til "suseptible"
+        self.suseptible.append(self.suseptible[-1] +
+                               ((-self.beta * self.suseptible[-1] * self.infected[-1]) / self.pop))
+        # Funksjonen for å finne ut ny S: S - (B * S * I)/Pop
+
+    def ny_I(self):  # Sette ny verdie til "infected"
+        self.infected.append(self.infected[-1] +
+                             (self.beta * self.suseptible[-2] * self.infected[-1]) / self.pop)
+        # - Y*self.infected[-1]))
+        # Funksjon for å finne ut ny I: I + (B * S * I)/Pop - (Y * I)
+
     # def ny_R(self): #Sette ny verdi til "recoverd"
     # self.recoverd.append(self.recoverd[-1] + (Y*self.infected[-2]))
     # Funksjon for å finne ut ny R: R + (Y * I)
-
-        self.posisjon = posisjon #Område navn. 
-        self.pop = sted_populasjon #Populasjonen til det område
-        self.beta = beta #Beta verdien er hvor ofte folk omgås i det område
-        self.suseptible = [sted_populasjon] #Hvor mange som er "Suseptible" i område
-        self.infected = [0] #Hvor mange som er "infected" i område
-        #self.recoverd = [0] #Hvor mange som har "recoverd" i område
-        
-    def ny_S(self): #Sette ny verdi til "suseptible"
-        self.suseptible.append(self.suseptible[-1] + 
-            ((-self.beta*self.suseptible[-1]*self.infected[-1])/self.pop))
-            #Funksjonen for å finne ut ny S: S - (B * S * I)/Pop
-        
-    def ny_I(self): #Sette ny verdie til "infected"
-        self.infected.append(self.infected[-1] + 
-            (self.beta*self.suseptible[-2]*self.infected[-1])/self.pop)
-            #- Y*self.infected[-1]))
-            #Funksjon for å finne ut ny I: I + (B * S * I)/Pop - (Y * I)
-    
-    #def ny_R(self): #Sette ny verdi til "recoverd"
-        #self.recoverd.append(self.recoverd[-1] + (Y*self.infected[-2]))
-            #Funksjon for å finne ut ny R: R + (Y * I)
-
