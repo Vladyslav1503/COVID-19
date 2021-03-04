76cee7cea237a40dbf212d1ef029ff3d154a9358
Vladyslav1503 <49617487+Vladyslav1503@users.noreply.github.com>
2021-03-04 23:45:22 +0100

    fix color range

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from src.preprocessing import image_preprocessing
+import cv2
 
 IMAGE_SRC = "./src/images/VerdenPopMindre.png"
+START_POINT = (530, 150)  # (659, 578)  # (800, 215)
 
 population_points, borders = image_preprocessing(IMAGE_SRC)
+img = cv2.imread(IMAGE_SRC)
+for i in population_points.keys():
+    x, y = i[0], i[1]
+    img[y, x] = (0, 0, 255)
+
+cv2.imshow('title',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,15 +2,17 @@ import cv2
 import numpy as np
 
 
-# TODO comments, fix border coordinates
+# TODO comments, fix border coordinates, fix color range
 def image_preprocessing(image: str):
     img = cv2.imread(image)
-    border_color = cv2.inRange(img, np.array([119, 82, 82]), np.array([149, 156, 156]))
-    population_by_colors = {'1 - 5': np.array([184, 203, 170]), '5 - 25': np.array([146, 152, 80]),
-                            '25 - 250': np.array([131, 105, 33]), '250 - 1000': np.array([136, 60, 39]),
-                            '1000 - 2000': np.array([139, 0, 0])}
+    border_color = cv2.inRange(img, np.array([119, 82, 82]), np.array([183, 203, 170]))
+    population_by_colors = {'1 - 5': (np.array([183, 203, 170]), np.array([208, 235, 222])),
+                            '5 - 25': (np.array([140, 140, 70]), np.array([166, 172, 110])),
+                            '25 - 250': (np.array([131, 105, 33]), np.array([150, 112, 33])),
+                            '250 - 1000': (np.array([133, 60, 39]), np.array([136, 65, 39])),
+                            '1000 - 2000': (np.array([102, 0, 0]), np.array([139, 64, 37]))}
 
-    population_mask_points = {key: cv2.findNonZero(cv2.inRange(img, value, value)).tolist() for key, value in
+    population_mask_points = {key: cv2.findNonZero(cv2.inRange(img, value[0], value[1])).tolist() for key, value in
                               population_by_colors.items()}
     reverse_population_points = {tuple(*i): key for key, value in population_mask_points.items() for i in value}
     borders = cv2.findNonZero(border_color).tolist()
